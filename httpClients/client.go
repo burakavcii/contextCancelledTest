d9e63129fafc28245797beb65e3d9b556e439166
Burak Avcı <burak.avci@deliveryhero.com>
2022-08-22 13:22:34 +0300

    add new test functions

--- httpClients/client.go
+++ httpClients/client.go
@@ -20,6 +20,10 @@ func NewHTTPContextClient() HTTPContextClient {
 	parsedUrl, _ := url.Parse(URL)
 	return HTTPContextClient{client: http.Client{}, url: parsedUrl}
 }
+func NewHTTPContextClient3(URL string) HTTPContextClient {
+	parsedUrl, _ := url.Parse(URL)
+	return HTTPContextClient{client: http.Client{}, url: parsedUrl}
+}
 
 func (c *HTTPContextClient) CallCancelBefore() error {
 	httpRequest := http.Request{URL: c.url, Method: "GET"}
@@ -64,6 +68,29 @@ func (c *HTTPContextClient) CallCancelBefore2() error {
 	return nil
 }
 
+func (c *HTTPContextClient) CallCancelBefore3() error {
+	httpRequest := http.Request{URL: c.url, Method: "GET"}
+	resp, cancelFunc, err := c.doRequestWithTimeout3(&httpRequest)
+	defer cancelFunc()
+	if err != nil {
+		log.Printf("error while doing request: %s\n", err)
+		return err
+	}
+	defer func() {
+		closeErr := resp.Body.Close()
+		if closeErr != nil {
+			log.Printf("error while closing body: %s\n", closeErr)
+		}
+	}()
+	time.Sleep(time.Millisecond * 1)
+	_, err = io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("error while reading body: %s\n", err)
+		return err
+	}
+	return nil
+}
+
 func (c *HTTPContextClient) doRequestWithTimeout(request *http.Request) (*http.Response, error) {
 	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Millisecond*3000)
 	*request = *request.WithContext(ctx)
@@ -75,6 +102,18 @@ func (c *HTTPContextClient) doRequestWithTimeout(request *http.Request) (*http.R
 	}
 
 	return resp, nil
+}
+
+func (c *HTTPContextClient) doRequestWithTimeout3(request *http.Request) (*http.Response, context.CancelFunc, error) {
+	ctx, cancelFunc := context.WithTimeout(context.Background(), time.Millisecond*3000)
+	*request = *request.WithContext(ctx)
+
+	resp, err := c.client.Do(request)
+	if err != nil {
+		return nil, cancelFunc, err
+	}
+
+	return resp, cancelFunc, nil
 }
 
 func (c *HTTPContextClient) CallCancelAfter() error {
--- httpClients/client_test.go
+++ httpClients/client_test.go
@@ -1,6 +1,11 @@
 package httpClients
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func Test_HTTPContextClient_CallCancelBefore(t *testing.T) {
 	t.Log("TestCancelBefore")
@@ -17,6 +22,30 @@ func Test_HTTPContextClient_CallCancelBefore2(t *testing.T) {
 	client := NewHTTPContextClient()
 
 	err := client.CallCancelBefore2()
+	if err != nil {
+		t.Fatalf("err: %s\n", err)
+	}
+}
+
+func Test_HTTPContextClient_CallCancelBefore3(t *testing.T) {
+	t.Log("TestCancelBefore")
+	client := NewHTTPContextClient3(URL)
+
+	err := client.CallCancelBefore3()
+	if err != nil {
+		t.Fatalf("err: %s\n", err)
+	}
+}
+
+func Test_HTTPContextClient_CallCancelBefore4(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Second * 15)
+	}))
+	defer ts.Close()
+	t.Log("TestCancelBefore")
+	client := NewHTTPContextClient3(ts.URL)
+
+	err := client.CallCancelBefore3()
 	if err != nil {
 		t.Fatalf("err: %s\n", err)
 	}
